e4ee49dc3852035ad479923abf8988534363531c
riccardobek <37105282+riccardobek@users.noreply.github.com>
2019-01-01 21:53:14 +0100

    Prima soluzione all'esercizio

--- src/main/java/pcd2018/exe2/DiffieHellman.java
+++ src/main/java/pcd2018/exe2/DiffieHellman.java
@@ -1,7 +1,9 @@
 package pcd2018.exe2;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * Classe da completare per l'esercizio 2.
@@ -29,19 +31,41 @@ public class DiffieHellman {
    * @return tutte le coppie di possibili segreti a,b
    */
   public List<Integer> crack(long publicA, long publicB) {
-    List<Integer> res = new ArrayList<Integer>();
-/*
-    for (long i=0;i<LIMIT;++i){
-      long alice = DiffieHellmanUtils.modPow(publicA,i,p);
-      for (long j=0;j<LIMIT;++j){
-        long bob = DiffieHellmanUtils.modPow(publicB,j,p);
-        if(alice==bob){
-          res.add((int)i);
-          res.add((int)j);
-        }
-      }
-    }
-*/
+    List<Integer> res = new ArrayList<>();
+
+    HashMap<Integer, Long> alice = new HashMap<>(
+      IntStream.rangeClosed(0, LIMIT)
+               .parallel()
+               .boxed()
+               .collect(Collectors.toMap(x -> x, x -> DiffieHellmanUtils.modPow(publicB, x, p)))
+      );
+
+    HashMap<Integer, Long> bob = new HashMap<>(
+      IntStream.rangeClosed(0, LIMIT)
+               .parallel()
+               .boxed()
+               .collect(Collectors.toMap(x -> x, x -> DiffieHellmanUtils.modPow(publicA, x, p)))
+      );
+
+    alice.entrySet()
+            .parallelStream()
+            .forEach( x -> {
+              Integer[] values = bob.entrySet()
+                          .parallelStream()
+                          .filter( y -> y.getValue().equals(x.getValue()))
+                          .map(Map.Entry::getKey)
+                          .toArray(Integer[]::new);
+
+              Arrays.stream(values)
+                    .parallel()
+                    .sorted()
+                    .forEach(val -> {
+                      synchronized (this) {
+                        res.add(x.getKey());
+                        res.add(val);}
+                    });
+            });
+
     return res;
   }
 }
